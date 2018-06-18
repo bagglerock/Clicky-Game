4364ea4a805feb0843e031378358f688e6c94eed
Oscar Villalta <bagglerock@gmail.com>
2018-06-18 12:23:45 -0400

    use the fisher yates shuffle and it works great now

--- src/App.js
+++ src/App.js
@@ -10,23 +10,46 @@ class App extends Component {
   state = {
     friends,
     chosenFriends: [],
-    count: 0
+    count: 0,
+    longestStreak: 0
   };
+
+  // Fisher-Yates shuffle
+  shuffle = array => {
+    let m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
 
   removeFriend = id => {
     if (this.state.chosenFriends.indexOf(id) === -1){
       this.state.chosenFriends.push(id);
-      console.log("initial count: " + this.state.count);
       this.setState({
         count: this.state.count + 1
       });
-      
+    } else  {
+      this.state.chosenFriends = [];
+      console.log(this.state.chosenFriends);
+      this.setState({
+        count: 0,
+        longestStreak: this.state.longestStreak + 1
+      })
     }
-    
-    
     // Filter this.state.friends for friends with an id not equal to the id being removed
     //const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
+    this.shuffle(this.state.friends);
     this.setState({ friends });
     console.log("after the condition: " + this.state.count);
   };
